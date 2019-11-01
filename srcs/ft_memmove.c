b0a8e4be995701cc1d7c83b658aeedaa658a2952
Abe Augustijn <aaugusti@f0r1s9.codam.nl>
2019-11-01 17:14:21 +0100

    Fixed memmove

--- srcs/ft_memmove.c
+++ srcs/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: abe <abe@student.codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/28 20:17:50 by abe            #+#    #+#                */
-/*   Updated: 2019/11/01 16:13:36 by aaugusti      ########   odam.nl         */
+/*   Updated: 2019/11/01 17:10:57 by aaugusti      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,26 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	unsigned char		*d;
-	const unsigned char	*s;
 	size_t				i;
 
 	i = 0;
-	d = dest;
-	s = src;
-	if (d < s)
+	if (dest < src)
 	{
 		i = 0;
 		while (i < n)
 		{
-			d[i] = s[i];
+			((unsigned char*)dest)[i] = ((unsigned char*)src)[i];
 			i++;
 		}
 	}
-	else if (d > s)
+	else if (dest > src)
 	{
-		i = n - 1;
-		while (--i > 0)
-			d[i - 1] = s[i - 1];
+		i = n;
+		while (i > 0)
+		{
+			((unsigned char*)dest)[i - 1] = ((unsigned char*)src)[i - 1];
+			i--;
+		}
 	}
 	return (dest);
 }
