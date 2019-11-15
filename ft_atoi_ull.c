2ef903f53429e91684bf79c5e0b84e16c3c6a73b
Abe Augustijn <abe.augustijn@hotmail.nl>
2019-11-15 20:45:20 +0100

    Feat: added some atoi functions
    Also cleaned up libft.h

---
+++ ft_atoi_ull.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi_ull.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaugusti <aaugusti@student.codam.nl>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/15 20:30:41 by abe               #+#    #+#             */
+/*   Updated: 2019/11/15 20:31:55 by abe              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static void			skip_ws(char **str)
+{
+	while ((**str == '\t' || **str == '\n' || **str == ' ' || **str == '\r' ||
+			**str == '\f' || **str == '\v') && **str)
+		(*str)++;
+	if (**str == '+')
+		(*str)++;
+}
+
+/*
+**	Convert a character string to an unsigned long long.
+**	- returns 0 if overflow
+**	- allows unlimited whitespace at the beginning of the string
+**	- allows a signle '+' sign at the beginning of the string
+*/
+
+unsigned long long	ft_atoi_ull(const char *str)
+{
+	unsigned long long	res;
+	unsigned long long	prev_res;
+
+	skip_ws((char **)&str);
+	res = 0;
+	while (*str)
+	{
+		prev_res = res;
+		if (!ft_isdigit(*str))
+			break ;
+		res *= 10;
+		res += *str - '0';
+		if (res < prev_res)
+			return (0);
+		str++;
+	}
+	return (res);
+}
