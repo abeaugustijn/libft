bae905d9a03e86f60e2817e2f6c7a576cc00a103
Abe Augustijn <abe.augustijn@hotmail.nl>
2019-11-15 13:31:37 +0100

    Fix: ft_split and ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,39 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: aaugusti <aaugusti@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/28 15:24:02 by aaugusti       #+#    #+#                */
-/*   Updated: 2019/11/04 19:40:45 by abe              ###   ########.fr       */
+/*   Updated: 2019/11/15 12:17:26 by aaugusti      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+size_t		ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t	i;
-	size_t	dst_len;
-	size_t	src_len;
+	size_t i;
+	size_t j;
 
-	dst_len = ft_strlen(dst);
-	src_len = ft_strlen(src);
-	if (dstsize == 0)
-		return (dst_len + src_len);
-	while (*dst)
-		dst++;
 	i = 0;
-	while (dst_len + i < dstsize - 1)
-	{
-		*dst = *src;
-		dst++;
-		src++;
+	j = 0;
+	while (dst[i])
 		i++;
+	while (j < dstsize - i - 1 && src[j] && dstsize > i)
+	{
+		dst[i + j] = src[j];
+		j++;
 	}
-	if (i < dstsize - dst_len && dstsize > 0 && dst_len < dstsize)
-		*dst = '\0';
-	return (((dst_len > dstsize) ? dstsize : dst_len) + src_len);
+	dst[i + j] = '\0';
+	while (src[j] != '\0')
+	{
+		j++;
+	}
+	if (dstsize < i)
+		return (j + dstsize);
+	return (i + j);
 }
