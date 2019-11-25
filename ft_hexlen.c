6fed53f36e3f1660893d0011bff0f13e65ece3e2
Abe Augustijn <abe.augustijn@hotmail.nl>
2019-11-25 15:34:26 +0100

    Feat: added ft_hexlen

--- ft_hexlen.c
+++ ft_hexlen.c
@@ -1,40 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_puthex_fd.c                                     :+:      :+:    :+:   */
+/*   ft_hexlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aaugusti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/22 13:24:10 by aaugusti          #+#    #+#             */
-/*   Updated: 2019/11/25 15:34:00 by aaugusti         ###   ########.fr       */
+/*   Created: 2019/11/25 15:29:26 by aaugusti          #+#    #+#             */
+/*   Updated: 2019/11/25 15:32:13 by aaugusti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
-#include <limits.h>
+#include <stddef.h>
 
-void		ft_puthex_fd(unsigned long long n, int fd, int cap)
+size_t	ft_hexlen(unsigned long long n)
 {
-	size_t	n_len;
-	char	res[17];
-	int		i;
-	char	*bases;
+	int	res;
 
-	bases = cap ? "0123456789ABCDEF" : "0123456789abcdef";
-	n_len = ft_hexlen(n);
+	res = 0;
 	if (n == 0)
+		return (1);
+	while (n > 0)
 	{
-		ft_putchar_fd('0', 1);
-		return ;
-	}
-	i = 0;
-	res[n_len] = '\0';
-	while (i < n_len)
-	{
-		res[n_len - 1 - i] = bases[n % 16];
 		n /= 16;
-		i++;
+		res++;
 	}
-	ft_putstr_fd(res, fd);
+	return (res);
 }
