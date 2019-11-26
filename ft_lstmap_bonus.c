5eaf2d23008a79af6b0c9656c8fc931a2383bf34
Abe Augustijn <abe.augustijn@hotmail.nl>
2019-11-26 15:48:02 +0100

    Fix: ft_lstmap_bonus

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: abe <abe@student.codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/29 20:15:57 by abe            #+#    #+#                */
-/*   Updated: 2019/11/04 19:39:15 by abe              ###   ########.fr       */
+/*   Updated: 2019/11/26 15:47:50 by aaugusti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,12 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	while (lst)
 	{
 		new_content = f(lst->content);
-		if (new_content == NULL)
-			del(lst->content);
 		new = ft_lstnew(new_content);
+		if (new == NULL)
+		{
+			ft_lstclear(&res, del);
+			return (NULL);
+		}
 		ft_lstadd_back(&res, new);
 		lst = lst->next;
 	}
