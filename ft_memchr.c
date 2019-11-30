c3aa797b4f0542f1addf2643c80010489cb34e47
Abe Augustijn <abe.augustijn@hotmail.nl>
2019-11-30 20:55:50 +0100

    Feat: improved memchr (needs TESTING)

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: abe <abe@student.codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/28 20:25:07 by abe            #+#    #+#                */
-/*   Updated: 2019/11/28 12:53:24 by aaugusti         ###   ########.fr       */
+/*   Updated: 2019/11/30 20:33:44 by aaugusti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,12 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
-
-	i = 0;
-	while (i < n)
+	while (n > 0)
 	{
-		if (((unsigned char *)s)[i] == (unsigned char)c)
-			return ((unsigned char *)s + i);
-		i++;
+		if (*((unsigned char *)s) == (unsigned char)c)
+			return ((void *)s);
+		s++;
+		n--;
 	}
 	return (NULL);
 }
